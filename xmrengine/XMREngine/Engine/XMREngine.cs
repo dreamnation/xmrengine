407be737ed8466f88cb1e4b82837717149c24e10
root <root@grid00002.t-data.com>
2009-11-26 01:22:08 +0100

    Flesh out the actual engine

--- xmrengine/XMREngine/Engine/XMREngine.cs
+++ xmrengine/XMREngine/Engine/XMREngine.cs
@@ -9,15 +9,18 @@
 using System;
 using System.IO;
 using System.Runtime.Remoting;
+using System.Reflection;
 using OpenSim.Framework;
 using OpenSim.Framework.Console;
 using OpenSim.Region.Framework.Scenes;
 using OpenSim.Region.Framework.Interfaces;
 using OpenSim.Region.ScriptEngine.Interfaces;
+using OpenSim.Region.ScriptEngine.Shared;
 using Nini.Config;
 using Mono.Addins;
 using MMR;
 using OpenMetaverse;
+using log4net;
 
 [assembly: Addin("XMREngine", "0.1")]
 [assembly: AddinDependency("OpenSim", "0.5")]
@@ -25,8 +28,17 @@ using OpenMetaverse;
 namespace OpenSim.Region.ScriptEngine.XMREngine
 {
     [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule", Id = "XMREngine")]
-    public class XMREngine : INonSharedRegionModule
+    public class XMREngine : INonSharedRegionModule, IScriptEngine,
+            IScriptModule
     {
+        private static readonly ILog m_log =
+            LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private Scene m_Scene;
+        private IConfigSource m_ConfigSource;
+        private IConfig m_Config;
+        private string m_ScriptBasePath;
+
         public XMREngine()
         {
         }
@@ -43,6 +55,13 @@ namespace OpenSim.Region.ScriptEngine.XMREngine
 
         public void Initialise(IConfigSource config)
         {
+            m_ConfigSource = config;
+
+            if (config.Configs["XMREngine"] == null)
+                m_Config = config.AddConfig("XMREngine");
+            else
+                m_Config = config.Configs["XMREngine"];
+
             MainConsole.Instance.Commands.AddCommand("xmr", false,
                     "xmr test",
                     "xmr test",
@@ -52,6 +71,12 @@ namespace OpenSim.Region.ScriptEngine.XMREngine
 
         public void AddRegion(Scene scene)
         {
+            m_Scene = scene;
+
+            m_ScriptBasePath = m_Config.GetString("ScriptBasePath",
+                    Path.Combine(".", "ScriptData"));
+            m_ScriptBasePath = Path.Combine(m_ScriptBasePath,
+                    scene.RegionInfo.RegionID.ToString());
         }
 
         public void RemoveRegion(Scene scene)
@@ -84,7 +109,165 @@ namespace OpenSim.Region.ScriptEngine.XMREngine
             fs.Close();
 
             ScriptCompile.Compile(text, "/tmp/assem.dll", UUID.Zero.ToString(),
-                    null);
+                    ErrorHandler);
+        }
+
+        private void ErrorHandler(Token token, string message)
+        {
+            m_log.DebugFormat("[MMR]: ({0},{1}) Error: {2}", token.line,
+                    token.posn, message);
+        }
+
+        // Not required when not using IScriptInstance
+        //
+        public IScriptWorkItem QueueEventHandler(object parms)
+        {
+            return null;
+        }
+
+        public Scene World
+        {
+            get { return m_Scene; }
+        }
+
+        public IScriptModule ScriptModule
+        {
+            get { return this; }
+        }
+
+        public event ScriptRemoved OnScriptRemoved;
+        public event ObjectRemoved OnObjectRemoved;
+
+        public bool PostScriptEvent(UUID itemID, EventParams parms)
+        {
+            return false;
+        }
+
+        public bool PostObjectEvent(uint localID, EventParams parms)
+        {
+            return false;
+        }
+
+        public DetectParams GetDetectParams(UUID item, int number)
+        {
+            return null;
+        }
+
+        public void SetMinEventDelay(UUID itemID, double delay)
+        {
+        }
+
+        public int GetStartParameter(UUID itemID)
+        {
+            return 0;
+        }
+
+        // This is the "set running" method
+        //
+        public void SetScriptState(UUID itemID, bool state)
+        {
+        }
+
+        // Control display of the "running" checkbox
+        //
+        public bool GetScriptState(UUID itemID)
+        {
+            return false;
+        }
+
+        public void SetState(UUID itemID, string newState)
+        {
+        }
+
+        public void ApiResetScript(UUID itemID)
+        {
+        }
+
+        public void ResetScript(UUID itemID)
+        {
+        }
+
+        public IConfig Config
+        {
+            get { return m_Config; }
+        }
+
+        public IConfigSource ConfigSource
+        {
+            get { return m_ConfigSource; }
+        }
+
+        public string ScriptEngineName
+        {
+            get { return "XMREngine"; }
+        }
+
+        public IScriptApi GetApi(UUID itemID, string name)
+        {
+            return null;
+        }
+
+        public string GetAssemblyName(UUID itemID)
+        {
+            return Path.Combine(m_ScriptBasePath, itemID.ToString() + ".dll");
+        }
+
+        public string GetXMLState(UUID itemID)
+        {
+            return String.Empty;
+        }
+
+        public bool CanBeDeleted(UUID itemID)
+        {
+            return true;
+        }
+
+        public bool PostScriptEvent(UUID itemID, string name, Object[] p)
+        {
+            Object[] lsl_p = new Object[p.Length];
+            for (int i = 0; i < p.Length ; i++)
+            {
+                if (p[i] is int)
+                    lsl_p[i] = new LSL_Types.LSLInteger((int)p[i]);
+                else if (p[i] is string)
+                    lsl_p[i] = new LSL_Types.LSLString((string)p[i]);
+                else if (p[i] is Vector3)
+                    lsl_p[i] = new LSL_Types.Vector3(((Vector3)p[i]).X, ((Vector3)p[i]).Y, ((Vector3)p[i]).Z);
+                else if (p[i] is Quaternion)
+                    lsl_p[i] = new LSL_Types.Quaternion(((Quaternion)p[i]).X, ((Quaternion)p[i]).Y, ((Quaternion)p[i]).Z, ((Quaternion)p[i]).W);
+                else if (p[i] is float)
+                    lsl_p[i] = new LSL_Types.LSLFloat((float)p[i]);
+                else
+                    lsl_p[i] = p[i];
+            }
+
+            return PostScriptEvent(itemID, new EventParams(name, lsl_p, new DetectParams[0]));
+        }
+
+        public bool PostObjectEvent(UUID itemID, string name, Object[] p)
+        {
+            SceneObjectPart part = m_Scene.GetSceneObjectPart(itemID);
+            if (part == null)
+                return false;
+
+            Object[] lsl_p = new Object[p.Length];
+            for (int i = 0; i < p.Length ; i++)
+            {
+                if (p[i] is int)
+                    lsl_p[i] = new LSL_Types.LSLInteger((int)p[i]);
+                else if (p[i] is string)
+                    lsl_p[i] = new LSL_Types.LSLString((string)p[i]);
+                else if (p[i] is Vector3)
+                    lsl_p[i] = new LSL_Types.Vector3(((Vector3)p[i]).X, ((Vector3)p[i]).Y, ((Vector3)p[i]).Z);
+                else if (p[i] is Quaternion)
+                    lsl_p[i] = new LSL_Types.Quaternion(((Quaternion)p[i]).X, ((Quaternion)p[i]).Y, ((Quaternion)p[i]).Z, ((Quaternion)p[i]).W);
+                else if (p[i] is float)
+                    lsl_p[i] = new LSL_Types.LSLFloat((float)p[i]);
+                else
+                    lsl_p[i] = p[i];
+            }
+
+            return PostObjectEvent(part.LocalId, new EventParams(name, lsl_p, new DetectParams[0]));
         }
     }
 }
