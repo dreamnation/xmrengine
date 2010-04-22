47cc6e71b9fa35395de76440d87b0dcabe557bc2
Melanie Thielker <melanie@t-data.com>
2010-04-22 12:16:43 +0200

    Add the attachments module

---
+++ xattachments/XAttachments/Module/XAttachments.cs
@@ -0,0 +1,113 @@
+////////////////////////////////////////////////////////////////
+//
+// (c) 2009, 2010 Careminster Limited and Melanie Thielker
+//
+// All rights reserved
+//
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Reflection;
+using OpenSim.Framework;
+using OpenSim.Framework.Console;
+using OpenSim.Server.Base;
+using OpenSim.Services.Base;
+using OpenSim.Services.Interfaces;
+using Nini.Config;
+using log4net;
+using Careminster;
+using OpenMetaverse;
+
+namespace Careminster
+{
+    public class XAttachments : ServiceBase, IAttachmentsService
+    {
+        private static readonly ILog m_log =
+                LogManager.GetLogger(
+                MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected string m_FSBase;
+
+        public XAttachments(IConfigSource config) : base(config)
+        {
+            MainConsole.Instance.Commands.AddCommand("fs", false,
+                    "delete attachments", "delete attachments <ID>",
+                    "Delete agent's attachments from server",
+                    HandleDeleteAttachments);
+
+            IConfig assetConfig = config.Configs["AttachmentsService"];
+            if (assetConfig == null)
+            {
+                throw new Exception("No AttachmentsService configuration");
+            }
+
+            m_FSBase = assetConfig.GetString("BaseDirectory", String.Empty);
+            if (m_FSBase == String.Empty)
+            {
+                m_log.ErrorFormat("[ATTACHMENTS]: BaseDirectory not specified");
+                throw new Exception("Configuration error");
+            }
+
+            m_log.Info("[ATTACHMENTS]: XAttachments service enabled");
+        }
+
+        public byte[] GetFsData(string id)
+        {
+            string file = HashToFile(id);
+            string diskFile = Path.Combine(m_FSBase, file);
+
+            if (File.Exists(diskFile))
+            {
+                try
+                {
+                    byte[] content = File.ReadAllBytes(diskFile);
+
+                    return content;
+                }
+                catch
+                {
+                }
+            }
+            return new Byte[0];
+        }
+
+        private void Store(string id, byte[] data)
+        {
+            string file = HashToFile(id);
+            string diskFile = Path.Combine(m_FSBase, file);
+
+            Directory.CreateDirectory(Path.GetDirectoryName(diskFile));
+
+            File.Delete(diskFile);
+
+            FileStream fs = File.Create(diskFile);
+
+            fs.Write(data, 0, data.Length);
+
+            fs.Close();
+        }
+
+        private void HandleDeleteAttachments(string module, string[] args)
+        {
+            if (args.Length < 3)
+            {
+                MainConsole.Instance.Output("Syntax: delete attachments <ID>");
+                return;
+            }
+
+            string file = HashToFile(args[2]);
+            string diskFile = Path.Combine(m_FSBase, file);
+
+            if (File.Exists(diskFile))
+            {
+                File.Delete(diskFile);
+                MainConsole.Instance.Output("Attachments deleted");
+                return;
+            }
+            MainConsole.Instance.Output("Attachments not found");
+        }
+    }
+}
