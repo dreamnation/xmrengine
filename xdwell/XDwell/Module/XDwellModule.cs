7099a7e95a328a90936e1fa3aa98487394b3a303
Melanie Thielker <melanie@t-data.com>
2010-09-06 01:32:49 +0200

    Implement the dwell functionality

--- xdwell/XDwell/Module/XDwellModule.cs
+++ xdwell/XDwell/Module/XDwellModule.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Timers;
+using System.Data;
+using MySql.Data.MySqlClient;
 using log4net;
 using Nini.Config;
 using Nwc.XmlRpc;
@@ -42,6 +45,7 @@ namespace Careminster.Modules.XDwell
 
         protected Scene m_Scene;
         protected string m_DatabaseConnect;
+        protected Timer m_DwellTimer = new Timer();
 
         public void Initialise(IConfigSource config)
         {
@@ -61,6 +65,11 @@ namespace Careminster.Modules.XDwell
 
             m_DwellTable = new DwellTableHandler("", "XDwell", String.Empty);
             m_log.Info("[XDwell]: Dwell module active");
+
+            m_DwellTimer.AutoReset = true;
+            m_DwellTimer.Interval = 600000; // 10 minutes
+            m_DwellTimer.Elapsed += OnDwellTimer;
+            m_DwellTimer.Start();
         }
 
         public void Close()
@@ -109,7 +118,68 @@ namespace Careminster.Modules.XDwell
 
         public int GetDwell(UUID parcelID)
         {
-            return 0;
+            XDwellData[] dwell = m_DwellTable.Get("ParcelID", parcelID.ToString());
+            if (dwell.Length < 1)
+                return 0;
+
+            return Convert.ToInt32(dwell[0].Data["AggregatedDwell"]);
+        }
+
+        private void OnDwellTimer(object sender, ElapsedEventArgs args)
+        {
+            List<ILandObject> landObjects = m_Scene.LandChannel.AllParcels();
+
+            if (landObjects == null || landObjects.Count == 0)
+                return;
+
+            List<string> parcelIDs = new List<string>();
+            Dictionary<UUID,int> counts = new Dictionary<UUID,int>();
+
+            foreach (ILandObject land in landObjects)
+            {
+                parcelIDs.Add(land.LandData.GlobalID.ToString());
+                counts[land.LandData.GlobalID] = 0;
+            }
+
+            string where = "ParcelID not in ('" + string.Join("', '", parcelIDs.ToArray()) + "')";
+            m_DwellTable.Delete(where);
+
+            m_Scene.ForEachScenePresence(delegate (ScenePresence p)
+                    {
+                        if (p.IsChildAgent)
+                            return;
+                        Vector3 pos = p.AbsolutePosition;
+                        ILandObject land = m_Scene.LandChannel.GetLandObject(
+                                pos.X, pos.Y);
+                        if (land == null)
+                            return;
+                        if (counts.ContainsKey(land.LandData.GlobalID))
+                            counts[land.LandData.GlobalID]++;
+                    });
+            foreach (KeyValuePair<UUID,int> kvp in counts)
+            {
+                if (kvp.Value == 0)
+                    continue;
+
+                XDwellData[] data = m_DwellTable.Get("ParcelID", kvp.Key.ToString());
+                if (data.Length == 0)
+                {
+                    data = new XDwellData[1];
+                    data[0] = new XDwellData();
+                    data[0].Data = new Dictionary<string, string>();
+
+                    data[0].ParcelID = kvp.Key;
+                    data[0].RegionID = m_Scene.RegionInfo.RegionID;
+                    data[0].Data["Dwell"] = "0";
+                    data[0].Data["AggregatedDwell"] = "0";
+                }
+
+                int dwell = Convert.ToInt32(data[0].Data["Dwell"]);
+                dwell += kvp.Value;
+                data[0].Data["Dwell"] = dwell.ToString();
+
+                m_DwellTable.Store(data[0]);
+            }
         }
     }
 
@@ -118,6 +188,17 @@ namespace Careminster.Modules.XDwell
         public DwellTableHandler(string conn, string realm, string migration) :
                 base(conn, realm, migration)
         {
+        }
+
+        public void Delete(string where)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.CommandText = "delete from " + m_Realm + " where " + where;
+            
+            ExecuteNonQuery(cmd);
+
+            cmd.Dispose();
         }
     }
 }
