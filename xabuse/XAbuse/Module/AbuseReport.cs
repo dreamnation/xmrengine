f8dc1a27ab1e80c34c835926cd73603c60b8b583
Melanie <melanie@t-data.com>
2010-11-18 05:05:08 +0100

    Implement abuse reporting

---
+++ xabuse/XAbuse/Module/AbuseReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenMetaverse;
+
+namespace Careminster.Modules.AbuseReport
+{
+    public class AbuseReport
+    {
+        public UUID ReportID;
+        public string Category;
+        public string ReporterName;
+        public string ObjectName;
+        public UUID ObjectUUID;
+        public string AbuserName;
+        public string AbuseLocation;
+        public string AbuseDetails;
+        public string ObjectPosition;
+        public string RegionName;
+        public UUID ScreenshotID;
+        public string AbuseSummary;
+        public int Number;
+        public string AssignedTo;
+        public bool Active;
+        public bool Checked;
+        public string Notes;
+    }
+}
