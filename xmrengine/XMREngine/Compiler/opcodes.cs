f62ef35079e339f59db892d39fc4c4461a66855f
Mike Rieker <mrieker@nii.net>
2012-08-16 09:16:34 -0400

    opcode table dump program opcodes.cs

---
+++ xmrengine/XMREngine/Compiler/opcodes.cs
@@ -0,0 +1,33 @@
+// dump opcode table
+//   gmcs -out:opcodes.exe opcodes.cs
+//   mono opcodes.exe
+
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+public class Opcodes {
+    public static void Main (string[] args)
+    {
+        FieldInfo[] opcodesFields = typeof (OpCodes).GetFields ();
+        foreach (FieldInfo opcodesField in opcodesFields) {
+            if (opcodesField.IsStatic && (opcodesField.FieldType == typeof (OpCode))) {
+                string name = opcodesField.Name;
+                OpCode opcode = (OpCode)opcodesField.GetValue (null);
+                /*
+                    public OpCodeType OpCodeType {
+                    public OperandType OperandType {
+                    public FlowControl FlowControl {
+                    public StackBehaviour StackBehaviourPop {
+                    public StackBehaviour StackBehaviourPush {
+                */
+                Console.WriteLine (name.PadRight (20) + 
+                        opcode.OpCodeType.ToString ().PadRight (12) + 
+                        opcode.OperandType.ToString ().PadRight (24) + 
+                        opcode.FlowControl.ToString ().PadRight (15) + 
+                        opcode.StackBehaviourPop.ToString ().PadRight (20) + 
+                        opcode.StackBehaviourPush.ToString ());
+            }
+        }
+    }
+}
