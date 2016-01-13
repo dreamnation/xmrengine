d52328900d09c5e481ad2e5c0f00497b89a55c2c
Kunta Kinte <kunta.kinte@dreamnation.net>
2016-01-13 12:09:59 -0800

    get more of xmrengtest working
    ...and fix some dependencies in make
    
    note:  seems mono has taken dynamic method il code offset info
           out of exception stack trace string

--- xmrengine/XMREngine/Compiler/findmonotasklet.sh
+++ xmrengine/XMREngine/Compiler/findmonotasklet.sh
@@ -3,7 +3,7 @@
 #  See if Mono.Tasklets.dll contains the given class definition
 #
 #    $1 = monobindir
-#    $2 = monodlldir
+#    $2 = tasklets dll filename
 #    $3 = class name
 #
 if [ ! -x $1/monodis ]
@@ -11,10 +11,10 @@ then
     echo 0
     exit
 fi
-if [ ! -f $2/Mono.Tasklets.dll ]
+if [ ! -f $2 ]
 then
     echo 0
     exit
 fi
-$1/monodis --typedef $2/Mono.Tasklets.dll | grep -q Mono.Tasklets.$3
+$1/monodis --typedef $2 | grep -q Mono.Tasklets.$3
 echo $(( $? == 0 ))
